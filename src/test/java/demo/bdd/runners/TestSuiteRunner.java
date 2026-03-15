edaef2fe08a61f36a6505d4a313078cc78e03b25
psaltafe <panagiotis.saltaferas@ext.euipo.europa.eu>
2026-03-16 00:07:12 +0200

    Comment out FILTER_TAGS_PROPERTY_NAME configuration in TestSuiteRunner

--- src/test/java/demo/bdd/runners/TestSuiteRunner.java
+++ src/test/java/demo/bdd/runners/TestSuiteRunner.java
@@ -13,6 +13,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:target/timeline,json:target/cucumber-reports/report.json,html:target/html-reports/report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "demo.bdd,stepdefinitions")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@UI")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@UI")
 public class TestSuiteRunner {
 }
